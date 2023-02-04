8548a1f4ee1e5d90103d734b691a2d9adc89eefd
EliezerAmbion <eliezerambion@gmail.com>
2023-02-04 13:40:51 +0800

    add futurebuilder to add and get the messages

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import '../screens/chat_screen.dart';
 
-void main() {
+import '../screens/chat_screen.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
